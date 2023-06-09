1f1f49584947bc14c46f91fb296dd718c4158f85
Victor Palma <palma.victor@gmail.com>
2023-06-09 16:16:30 -0500

    update aws client to fix eks invalid apiVersion error

--- 01-setup/install.sh
+++ 01-setup/install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# upgrade aws otherwise we will not be able to talk to our eks cluster
+pip3 install ^Cscli --upgrade --user
+echo 'export PATH=~/.local/bin:$PATH' >>~/.bashrc
+source ~/.bashrc
+
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo mv kubectl /usr/local/bin/
 sudo chmod +x /usr/local/bin/kubectl
@@ -12,3 +17,6 @@ sudo mv helmfile /usr/local/bin/
 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
+
+echo "To finalize the setup source the bashrc file you can do that by executing the following command."
+echo "source ~/.bashrc"
